5fef00e722b393938db1e401f5ea04fc34f7b972
Akshat-Bh <118525576+Akshat-Bh@users.noreply.github.com>
2023-06-01 17:07:04 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
+scoreRighttWrist= 0;
+right_wristX= 0;
+right_wristY = 0;
+
 function setup() {
 	canvas = createCanvas(1240,336);
     canvas.parent('canvas');
@@ -13,4 +17,23 @@ function modelLoaded()
     console.log('Model Loaded!');
 }
 
-function gotPoses
+function gotPoses(results)
+  {
+	if(results.length > 0)
+	{
+      rightWristX= results[0].pose.rightWrist.x;
+      rightWristY= results[0].pose.rightWrist.y;
+
+      scoreRighttWrist= results[0].pose.keypoint[10].score;
+      console.log("scoreRightWrist = " + scoreRightWrist);
+
+	  console.log(results);
+	}
+  }
+
+  if(scoreRightWrist > 0.2)
+    {
+    fill("#FFFF00");
+    stroke("#FFFF00");
+    circle(rightWristX, rightWristY, 20);
+    }
